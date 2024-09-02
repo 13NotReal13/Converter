dae1cd2d4dda6682c423d0d507ddf47255828c00
Ivan Semikin <i.semikin.94@icloud.com>
2024-09-02 11:39:39 +0200

    Did finish the project

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -7,15 +7,51 @@
 
 import SwiftUI
 
+import SwiftUI
+import Foundation
+
 struct ContentView: View {
+    @State private var inputValue: Double = 0
+    @State private var inputType: UnitLength = .centimeters
+    @State private var outputType: UnitLength = .kilometers
+    
+    private let typesOfLengths: [UnitLength] = [.meters, .kilometers, .feet, .yards, .miles]
+    
+    // Computed property that handles conversion
+    var convertedValue: Double {
+        let inputMeasurement = Measurement(value: inputValue, unit: inputType)
+        let outputMeasurement = inputMeasurement.converted(to: outputType)
+        return outputMeasurement.value
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Enter The Value") {
+                    TextField("Value", value: $inputValue, format: .number)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section {
+                    Picker("Select Input Unit", selection: $inputType) {
+                        ForEach(typesOfLengths, id: \.self) {
+                            Text($0.symbol)
+                        }
+                    }
+                    
+                    Picker("Select Output Unit", selection: $outputType) {
+                        ForEach(typesOfLengths, id: \.self) {
+                            Text($0.symbol)
+                        }
+                    }
+                }
+                
+                Section {
+                    Text("Converted Value: \(convertedValue, format: .number) \(outputType.symbol)")
+                }
+            }
+            .navigationTitle("Converter")
         }
-        .padding()
     }
 }
 
