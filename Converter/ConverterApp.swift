b0c72338ee908a8df43346300d281c06a7f79fa7
Ivan Semikin <i.semikin.94@icloud.com>
2024-09-02 10:43:26 +0200

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Иван Семикин on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
